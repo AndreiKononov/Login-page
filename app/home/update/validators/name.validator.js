1249128015c8bd0113b35af1dbaa5f61502ef733
Andrei Konanau <Andrei_Konanau@epam.com>
2018-11-01 18:02:19 +0300

    Updated user name validator

--- app/home/update/validators/name.validator.js
+++ app/home/update/validators/name.validator.js
@@ -1,21 +1,20 @@
 (function () {
   'use strict';
   var app = angular.module('app');
+  var USERNAME_FORMAT_REGEXP1 = /^(([A-Z][a-z]+)+([A-Z])?)$/;
+  var USERNAME_FORMAT_REGEXP2 = /^(([A-Z][a-z]+)+([A-Z])? ([A-Z][a-z]+)+([A-Z])?)$/;
+
   app.directive('username', function($q, $timeout) {
     return {
       require: 'ngModel',
       link: function(scope, elm, attrs, ctrl) {
-        var usernames = ['Jim', 'John', 'Jill', 'Jackie'];
-
         ctrl.$asyncValidators.username = function(modelValue, viewValue) {
-
           if (ctrl.$isEmpty(modelValue)) {
             return $q.resolve();
           }
           var def = $q.defer();
-
           $timeout(function() {
-            if (usernames.indexOf(modelValue) === -1) {
+            if (USERNAME_FORMAT_REGEXP1.test(viewValue) || USERNAME_FORMAT_REGEXP2.test(viewValue)) {
               def.resolve();
             } else {
               def.reject();
